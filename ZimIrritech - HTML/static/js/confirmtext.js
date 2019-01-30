9d2fa03cd5e2eec5efb0a0cc9c1d8d774121f8cc
Mr Machiridza <michaelmachiridza@gmail.com>
2019-01-30 16:04:03 +0200

    first commit

---
+++ ZimIrritech - HTML/static/js/confirmtext.js
@@ -0,0 +1,8 @@
+function confirmText(){
+    document.getElementById("subscribe").innerHTML = "You have successfully been subscribed!";
+}
+
+document.getElementById("contact").onclick = function(){
+        alert("Done!!");
+    }  
+
